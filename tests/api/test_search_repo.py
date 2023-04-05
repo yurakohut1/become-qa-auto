ef8101f8f353f29c1c4f69e17513d9c8faf0d646
Yura <yura.kogut1@gmail.com>
2023-04-05 13:35:36 +0300

    Modified api tests

--- tests/api/test_search_repo.py
+++ tests/api/test_search_repo.py
@@ -1,4 +1,12 @@
+import requests
+
 def test_existing_repo_can_be_found():
     pass
+
+
 def test_non_existing_repo_can_be_found():
+    r = github_api_client.search_repo("smdnfbsmdfbsmfdsbfmsdb")
+    assert r['total_count'] ==0
+
+def test_search_not_working_without_q():
     pass
